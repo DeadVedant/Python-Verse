9d3fa081100fea21cffe8b3645843ac0a958b043
DeadVedant <232570915+VedantBakre@users.noreply.github.com>
2026-08-11 17:49:08 +0530

    Added Simple Python Patterns

--- 03-Visuals-and-Patterns/SimplePatterns.py
+++ 03-Visuals-and-Patterns/SimplePatterns.py
@@ -57,4 +57,25 @@ def inverted_half_pyramid(n):
         print("\r")
 
 inverted_half_pyramid(5)
+print()
+
+print("5.Side Ways")
+print()
+def half_pyramid(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print("")
+
+half_pyramid(5)
+
+def inverted_half_pyramid(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print("\r")
+
+inverted_half_pyramid(4)
+
+
 
